5bba91cf076fa5863a2ea19642dbdcc6c3479245
Tanvir islam <tanvirislam4147@gmail.com>
2025-10-07 17:27:48 +0600

    deploy

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const propertiesCollection = client.db("ezrent").collection("properties");
     const bookinghotelCollection = client
@@ -408,7 +408,7 @@ app.delete("/properties/:id", async (req, res) => {
     });
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
